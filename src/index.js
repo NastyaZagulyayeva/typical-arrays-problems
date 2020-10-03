56ed3a697975e6653300944fdd78707dfc3f9f75
Anastasiia Zahuliaieva <nastyazagyl2@gmail.com>
2020-10-03 19:49:29 +0300

    feat: Resolved min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (!Array.isArray(array) || (Array.isArray(array) && array.length < 1)) {
+    return 0;
+  }
+  let a = Math.min(...array);
+  return a;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (!Array.isArray(array) || (Array.isArray(array) && array.length < 1)) {
+    return 0;
+  }
+  let a = Math.max(...array);
+  return a;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (!Array.isArray(array) || (Array.isArray(array) && array.length < 1)) {
+    return 0;
+  }
+  return array.reduce((acc, value) => {
+    acc += value;
+    return acc;
+  }, 0) / array.length;
 }
